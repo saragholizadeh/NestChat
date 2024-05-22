bd2b37cdb10be07c6f9a1d89d6d99413e1a5c2b7
saragholizadeh <saragholizadeh1999@gmail.com>
2024-05-22 20:17:45 +0330

    add: swagger package & config in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 dotenv.config({
   path: '.env',
@@ -12,6 +13,19 @@ async function bootstrap() {
   const logger = new Logger('NestChat');
   const port = +process.env.PORT || 5000;
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestChat')
+    .addBearerAuth({
+      name: 'authorization',
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger/api/document', app, document);
+
   await app.listen(port, () =>
     logger.log(
       `Server running at port: ${port} on ${process.env.ENVIROMENT} environment`,
