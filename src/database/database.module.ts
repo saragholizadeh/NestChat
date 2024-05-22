1eb1d3fa8b202bf410a4996d42f4c73c158909a8
saragholizadeh <saragholizadeh1999@gmail.com>
2024-05-22 17:51:13 +0330

    Add: prettier to package.json

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,8 @@
-import {Module} from '@nestjs/common';
-import {databaseProviders} from "./database.provider";
+import { Module } from '@nestjs/common';
+import { databaseProviders } from './database.provider';
 
 @Module({
-    providers: [...databaseProviders],
-    exports: [...databaseProviders],
+  providers: [...databaseProviders],
+  exports: [...databaseProviders],
 })
-export class DatabaseModule {
-}
+export class DatabaseModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {Logger} from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import * as dotenv from 'dotenv';
-import {NestExpressApplication} from "@nestjs/platform-express";
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 dotenv.config({
   path: '.env',
@@ -12,7 +12,11 @@ async function bootstrap() {
   const logger = new Logger('NestChat');
   const port = +process.env.PORT || 5000;
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(port, () => logger.log(`Server running at port: ${port} on ${process.env.ENVIROMENT} environment`));
+  await app.listen(port, () =>
+    logger.log(
+      `Server running at port: ${port} on ${process.env.ENVIROMENT} environment`,
+    ),
+  );
 }
 
 bootstrap();
